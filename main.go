00229d3743e08281087dee03ff09e35f98a07b14
Andrés Pérez <andresxz09@gmail.com>
2017-03-03 10:50:36 -0500

    Connect to mongo and setup http server

--- main.go
+++ main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+
+	"github.com/Oxyrus/ChinguCentral/controllers"
+	"github.com/julienschmidt/httprouter"
+	mgo "gopkg.in/mgo.v2"
+)
 
 func main() {
-	fmt.Println("This is going to be awesome")
+	session, err := mgo.Dial("localhost")
+	if err != nil {
+		panic(err)
+	}
+	defer session.Close()
+
+	session.SetMode(mgo.Monotonic, true)
+
+	router := httprouter.New()
+
+	// router.GET("/", rootHandler(session))
+	// router.GET("/cohorts", cohortsHandler(session))
+	// router.GET("/cohorts/:name", cohortHandler(session))
+	router.GET("/users", controllers.AllUsers(session))
+	// router.GET("/users/:name", userHandler(session))
+
+	log.Fatal(http.ListenAndServe(":8080", router))
 }
