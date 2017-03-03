4901145c8b4508c72748bb7a4519d1b95707afd2
Andrés Pérez <andresxz09@gmail.com>
2017-03-03 10:49:42 -0500

    Create utilities

---
+++ utils/utils.go
@@ -0,0 +1,20 @@
+package utils
+
+import (
+	"fmt"
+	"net/http"
+)
+
+// ErrorWithJSON simplifies returning a JSON error
+func ErrorWithJSON(w http.ResponseWriter, message string, code int) {
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	w.WriteHeader(code)
+	fmt.Fprintf(w, "{message: %q}", message)
+}
+
+// ResponseWithJSON simplifies returning a successful response
+func ResponseWithJSON(w http.ResponseWriter, json []byte, code int) {
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	w.WriteHeader(code)
+	w.Write(json)
+}
