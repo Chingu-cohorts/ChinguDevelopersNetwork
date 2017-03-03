a644d5c631ed16a17b7069c432893e63ce528860
Andrés Pérez <andresxz09@gmail.com>
2017-03-03 10:50:16 -0500

    Start writting controllers for users

---
+++ controllers/userController.go
@@ -0,0 +1,37 @@
+package controllers
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+
+	"github.com/Oxyrus/ChinguCentral/models"
+	"github.com/Oxyrus/ChinguCentral/utils"
+	"github.com/julienschmidt/httprouter"
+	mgo "gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
+)
+
+// AllUsers returns a list of all the records in the database
+func AllUsers(s *mgo.Session) func(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	return func(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+		session := s.Copy()
+		defer session.Close()
+
+		c := session.DB("chingucentral").C("users")
+
+		var users []models.User
+
+		err := c.Find(bson.M{}).All(&users)
+		if err != nil {
+			utils.ErrorWithJSON(w, "Database error", http.StatusInternalServerError)
+		}
+
+		respBody, err := json.MarshalIndent(users, "", " ")
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		utils.ResponseWithJSON(w, respBody, http.StatusOK)
+	}
+}
