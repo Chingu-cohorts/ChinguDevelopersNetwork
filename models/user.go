664dbbd33065730a15bc8515cedb1e85c083f770
Andrés Pérez <andresxz09@gmail.com>
2017-03-04 17:48:55 -0500

    Fix user model

--- models/user.go
+++ models/user.go
@@ -4,7 +4,7 @@ import "gopkg.in/mgo.v2/bson"
 
 // User represents any user of the site
 type User struct {
-	ID    bson.ObjectId `json:"id" bson:"_id"`
-	Name  string        `json:"name" bson:"name"`
-	Email string        `json:"email" bson:"email"`
+	ID    bson.ObjectId `json:"id,omitempty" bson:"_id,omitempty"`
+	Name  string        `json:"name,omitempty" bson:"name,omitempty"`
+	Email string        `json:"email,omitempty" bson:"email,omitempty"`
 }
