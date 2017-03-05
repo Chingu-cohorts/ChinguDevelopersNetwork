098bd60cb69571ba51e4ff5e41d60c094859e944
Oxyrus <im.oxyrus@gmail.com>
2017-03-05 18:55:34 -0500

    Fix created at field

--- models/cohort.go
+++ models/cohort.go
@@ -1,11 +1,15 @@
 package models
 
-import "gopkg.in/mgo.v2/bson"
+import (
+	"time"
+
+	"gopkg.in/mgo.v2/bson"
+)
 
 // Cohort represents any cohort of the site
 type Cohort struct {
 	ID          bson.ObjectId `json:"id,omitempty" bson:"_id,omitempty"`
 	Name        string        `json:"name" bson:"name"`
 	Description string        `json:"description" bson:"description"`
-	CreatedAt   *time.Time    `json:"created_at" bson:"created_at"`
+	CreatedAt   time.Time     `json:"created_at" bson:"created_at"`
 }
--- models/user.go
+++ models/user.go
@@ -1,11 +1,15 @@
 package models
 
-import "gopkg.in/mgo.v2/bson"
+import (
+	"time"
+
+	"gopkg.in/mgo.v2/bson"
+)
 
 // User represents any user of the site
 type User struct {
 	ID        bson.ObjectId `json:"id,omitempty" bson:"_id,omitempty"`
 	Name      string        `json:"name,omitempty" bson:"name,omitempty"`
 	Email     string        `json:"email,omitempty" bson:"email,omitempty"`
-	CreatedAt *time.Time    `json:"created_at" bson:"created_at"`
+	CreatedAt time.Time     `json:"created_at" bson:"created_at"`
 }
