27896a66e5120fa84092ec216b4b645b5f9bfa40
Oxyrus <im.oxyrus@gmail.com>
2017-03-05 18:26:11 -0500

    Add created at field

--- models/cohort.go
+++ models/cohort.go
@@ -4,7 +4,8 @@ import "gopkg.in/mgo.v2/bson"
 
 // Cohort represents any cohort of the site
 type Cohort struct {
-	ID          bson.ObjectId `json:"id" bson:"_id"`
+	ID          bson.ObjectId `json:"id,omitempty" bson:"_id,omitempty"`
 	Name        string        `json:"name" bson:"name"`
 	Description string        `json:"description" bson:"description"`
+	CreatedAt   *time.Time    `json:"created_at" bson:"created_at"`
 }
--- models/user.go
+++ models/user.go
@@ -4,7 +4,8 @@ import "gopkg.in/mgo.v2/bson"
 
 // User represents any user of the site
 type User struct {
-	ID    bson.ObjectId `json:"id,omitempty" bson:"_id,omitempty"`
-	Name  string        `json:"name,omitempty" bson:"name,omitempty"`
-	Email string        `json:"email,omitempty" bson:"email,omitempty"`
+	ID        bson.ObjectId `json:"id,omitempty" bson:"_id,omitempty"`
+	Name      string        `json:"name,omitempty" bson:"name,omitempty"`
+	Email     string        `json:"email,omitempty" bson:"email,omitempty"`
+	CreatedAt *time.Time    `json:"created_at" bson:"created_at"`
 }
