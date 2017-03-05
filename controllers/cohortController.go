8cf6f416c94a397ab53a81c7ad399da4af78f3d8
Oxyrus <im.oxyrus@gmail.com>
2017-03-05 17:31:43 -0500

    Create basic controller for cohorts

---
+++ controllers/cohortController.go
@@ -0,0 +1,68 @@
+package controllers
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+
+	"github.com/Oxyrus/ChinguCentral/models"
+	"github.com/Oxyrus/ChinguCentral/utils"
+	"github.com/julienschmidt/httprouter"
+	mgo "gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
+)
+
+// AllCohorts returns a list of the cohorts in the database
+func AllCohorts(s *mgo.Session) func(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	return func(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+		session := s.Copy()
+		defer session.Close()
+
+		c := session.DB("caronte").C("cohorts")
+
+		var cohorts []models.Cohort
+		err := c.Find(nil).All(&cohorts)
+		if err != nil {
+			utils.ErrorWithJSON(w, "Database error", http.StatusInternalServerError)
+		}
+
+		respBody, err := json.MarshalIndent(cohorts, "", " ")
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		utils.ResponseWithJSON(w, respBody, http.StatusOK)
+	}
+}
+
+// CohortByName finds a cohort given a parameter with its name
+func CohortByName(s *mgo.Session) func(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	return func(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+		session := s.Copy()
+		defer session.Close()
+
+		name := ps.ByName("name")
+		var cohort models.Cohort
+
+		c := session.DB("caronte").C("cohorts")
+
+		err := c.Find(bson.M{"name": name}).One(&cohort)
+		if err != nil {
+			utils.ErrorWithJSON(w, "Database error", http.StatusInternalServerError)
+			log.Println("There was an error with the request", err)
+			return
+		}
+
+		if cohort.Name == "" {
+			utils.ErrorWithJSON(w, "Cohort not found", http.StatusNotFound)
+			return
+		}
+
+		respBody, err := json.MarshalIndent(cohort, "", " ")
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		utils.ResponseWithJSON(w, respBody, http.StatusOK)
+	}
+}
