5cd531876d0f2ff1db1b253bf62109aaa5a132a4
Oxyrus <im.oxyrus@gmail.com>
2017-03-05 17:05:20 -0500

    Return formatted JSON error, closes #1

--- utils/utils.go
+++ utils/utils.go
@@ -1,15 +1,27 @@
 package utils
 
 import (
-	"fmt"
+	"encoding/json"
 	"net/http"
+
+	"github.com/Oxyrus/ChinguCentral/models"
 )
 
 // ErrorWithJSON simplifies returning a JSON error
 func ErrorWithJSON(w http.ResponseWriter, message string, code int) {
 	w.Header().Set("Content-Type", "application/json; charset=utf-8")
 	w.WriteHeader(code)
-	fmt.Fprintf(w, "{\"message\": %q}", message)
+	var msg = models.CustomMessage{
+		Code:    code,
+		Message: message,
+	}
+
+	respBody, err := json.MarshalIndent(msg, "", " ")
+	if err != nil {
+		panic(err)
+	}
+
+	w.Write([]byte(respBody))
 }
 
 // ResponseWithJSON simplifies returning a successful response
