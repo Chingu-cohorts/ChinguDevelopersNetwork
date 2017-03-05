c6eeebeaaff7481aa80599c50d9471f21d96152f
Oxyrus <im.oxyrus@gmail.com>
2017-03-05 18:56:02 -0500

    Use negroni, create new routes

--- main.go
+++ main.go
@@ -3,9 +3,11 @@ package main
 import (
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/Oxyrus/ChinguCentral/controllers"
 	"github.com/julienschmidt/httprouter"
+	"github.com/urfave/negroni"
 	mgo "gopkg.in/mgo.v2"
 )
 
@@ -21,12 +23,23 @@ func main() {
 	router := httprouter.New()
 
 	// router.GET("/", rootHandler(session))
-	// router.GET("/cohorts", cohortsHandler(session))
-	// router.GET("/cohorts/:name", cohortHandler(session))
+	router.GET("/cohorts", controllers.AllCohorts(session))
+	router.GET("/cohorts/:name", controllers.CohortByName(session))
+	router.POST("/cohorts", controllers.CreateCohort(session))
 	router.GET("/users", controllers.AllUsers(session))
 	router.POST("/users", controllers.CreateUser(session))
 	router.GET("/users/:name", controllers.UserByName(session))
-	// router.GET("/users/:name", userHandler(session))
 
-	log.Fatal(http.ListenAndServe(":8080", router))
+	n := negroni.Classic()
+	n.UseHandler(router)
+
+	s := &http.Server{
+		Addr:           ":8080",
+		Handler:        n,
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+	}
+
+	log.Fatal(s.ListenAndServe())
 }
