63a816845ad1971854e9c3624c1c9a027f10553b
Johnny Zabala <jzabala.s@gmail.com>
2017-03-06 21:50:22 -0400

    Add yarn and test for header component

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -1,14 +1,13 @@
 import React, { Component } from 'react';
-import Header from './header/Header';
+import Header from '../Header/Header';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">        
+      <div className="App">
         <Header />
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
     );
