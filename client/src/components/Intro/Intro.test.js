9e7bc83c0b2df050c8a8241c936f6443bc80f041
oxyrus <andresxz09@gmail.com>
2017-03-26 21:56:51 +0000

    Generate Rails application

--- client/src/components/Intro/Intro.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { render } from 'react-dom';
-import Intro from './Intro';
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  render(<Intro />, div);
-});
