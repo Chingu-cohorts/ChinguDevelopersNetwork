91a64f304a89a5d855a2a42615ad94cd61a3c76f
oxyrus <andresxz09@gmail.com>
2017-04-15 21:39:19 +0000

    Add query to find by id

--- lib/chingu_central/web/schema.ex
+++ lib/chingu_central/web/schema.ex
@@ -7,20 +7,45 @@ defmodule ChinguCentral.Schema do
       resolve &ChinguCentral.Accounts.UserResolver.all/2
     end
     
+    field :accounts_user, type: :accounts_user do
+      arg :id, non_null(:id)
+      resolve &ChinguCentral.Accounts.UserResolver.find/2
+    end
+    
     field :forums_posts, list_of(:forums_post) do
       resolve &ChinguCentral.Forums.PostResolver.all/2
+    end
+    
+    field :forums_post, type: :forums_post do
+      arg :id, non_null(:id)
+      resolve &ChinguCentral.Forums.PostResolver.find/2
     end
     
     field :forums_categories, list_of(:forums_category) do
       resolve &ChinguCentral.Forums.CategoryResolver.all/2
     end
     
+    field :forums_category, type: :forums_category do
+      arg :id, non_null(:id)
+      resolve &ChinguCentral.Forums.CategoryResolver.find/2
+    end
+    
     field :forums_comments, list_of(:forums_comment) do
       resolve &ChinguCentral.Forums.CommentResolver.all/2
     end
     
+    field :forums_comment, type: :forums_comment do
+      arg :id, non_null(:id)
+      resolve &ChinguCentral.Forums.CommentResolver.find/2
+    end
+    
     field :applications_cohorts, list_of(:applications_cohort) do
       resolve &ChinguCentral.Applications.CohortResolver.all/2
+    end
+    
+    field :applications_cohort, type: :applications_cohort do
+      arg :id, non_null(:id)
+      resolve &ChinguCentral.Applications.CohortResolver.find/2
     end
   end
 end
