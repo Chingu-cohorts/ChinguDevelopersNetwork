cfb1206ab7d5c06d3b2e89235833d04ebd18637b
oxyrus <andresxz09@gmail.com>
2017-04-15 02:26:35 +0000

    Create basic types for the data models

--- lib/chingu_central/web/schema/types.ex
+++ lib/chingu_central/web/schema/types.ex
@@ -8,6 +8,14 @@ defmodule ChinguCentral.Schema.Types do
     field :email, :string
     field :username, :string
     field :forums_posts, list_of(:forums_post), resolve: assoc(:forums_posts)
+    field :applications_cohorts, list_of(:applications_cohort), resolve: assoc(:applications_cohorts)
+  end
+  
+  object :applications_cohort do
+    field :id, :id
+    field :name, :string
+    field :description, :string
+    field :accounts_users, list_of(:accounts_user), resolve: assoc(:accounts_users)
   end
   
   object :forums_post do
@@ -15,5 +23,12 @@ defmodule ChinguCentral.Schema.Types do
     field :title, :string
     field :content, :string
     field :accounts_user, :accounts_user, resolve: assoc(:accounts_user)
+    field :forums_category, :forums_category, resolve: assoc(:forums_category)
+  end
+  
+  object :forums_category do
+    field :id, :id
+    field :name, :string
+    field :description, :string
   end
 end
