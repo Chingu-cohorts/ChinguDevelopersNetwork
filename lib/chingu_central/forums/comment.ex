3e3bc2e384681ccc52936c0b70ec6552247175f6
oxyrus <andresxz09@gmail.com>
2017-04-15 02:30:41 +0000

    Fix relationship in comment

--- lib/chingu_central/forums/comment.ex
+++ lib/chingu_central/forums/comment.ex
@@ -4,10 +4,8 @@ defmodule ChinguCentral.Forums.Comment do
   schema "forums_comments" do
     field :content, :string
     
-    belongs_to :accounts_users, ChinguCentral.Accounts.User
-    belongs_to :forums_posts, ChinguCentral.Forums.Post
-    # field :user_id, :id
-    # field :post_id, :id
+    belongs_to :accounts_users, ChinguCentral.Accounts.User, foreign_key: :accounts_users_id
+    belongs_to :forums_posts, ChinguCentral.Forums.Post, foreign_key: :forums_posts_id
 
     timestamps()
   end
