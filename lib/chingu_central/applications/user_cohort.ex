2bc619d76ea54a00c18db0ad9e5b253ea449f6ac
oxyrus <andresxz09@gmail.com>
2017-04-15 02:29:36 +0000

    Use foreign keys instead

--- lib/chingu_central/applications/user_cohort.ex
+++ lib/chingu_central/applications/user_cohort.ex
@@ -2,8 +2,8 @@ defmodule ChinguCentral.Applications.UserCohort do
   use Ecto.Schema
 
   schema "applications_user_cohorts" do
-    belongs_to :accounts_users, ChinguCentral.Accounts.User
-    belongs_to :applications_cohorts, ChinguCentral.Applications.Cohort
+    belongs_to :accounts_users, ChinguCentral.Accounts.User, foreign_key: :accounts_users_id
+    belongs_to :applications_cohorts, ChinguCentral.Applications.Cohort, foreign_key: :applications_cohorts_id
 
     timestamps()
   end
