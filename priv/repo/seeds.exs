6cb3de1a89880ecf443d311d19d51e620fde3c64
oxyrus <andresxz09@gmail.com>
2017-04-15 02:24:30 +0000

    Seed the database properly

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -1,11 +1,31 @@
 alias ChinguCentral.Accounts.User
-alias ChinguCentral.Applications.Cohort
+alias ChinguCentral.Applications.{Cohort, UserCohort}
 alias ChinguCentral.Forums.{Category, Post, Comment}
 alias ChinguCentral.Repo
 
+# Seed cohorts
+Repo.insert!(%Cohort{name: "Penguin", description: "Penguins cohort"})
+Repo.insert!(%Cohort{name: "Red Panda", description: "Red Pandas cohort"})
+Repo.insert!(%Cohort{name: "Flamingo", description: "Flamingos cohort"})
+Repo.insert!(%Cohort{name: "Rhino", description: "Rhinos cohort"})
+
 # Seed users
-Repo.insert!(%User{name: "Jon Doe", username: "jondoe", email: "jon@doe.com", encrypted_password: "12345"})
-Repo.insert!(%User{name: "Jana Doe", username: "janadoe", email: "jana@doe.com", encrypted_password: "12345"})
+for _ <- 1..20 do
+  Repo.insert!(%User{
+    name: Faker.Name.name,
+    username: Faker.Name.first_name,
+    email: Faker.Name.first_name <> "@" <> "gmail.com",
+    encrypted_password: "123456"
+  })
+end
+
+# Seed users to cohorts
+for id <- 1..20 do
+  Repo.insert!(%UserCohort{
+    accounts_users_id: id,
+    applications_cohorts_id: Enum.random(1..4)
+  })
+end
 
 # Seed forum categories
 for _ <- 1..5 do
