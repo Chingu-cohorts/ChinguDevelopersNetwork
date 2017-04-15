1b9db31be9b11a52dbc69b32401c92442744c8b0
oxyrus <andresxz09@gmail.com>
2017-04-15 02:28:29 +0000

    Create basic schema for GraphQL

--- lib/chingu_central/web/schema.ex
+++ lib/chingu_central/web/schema.ex
@@ -10,5 +10,13 @@ defmodule ChinguCentral.Schema do
     field :forums_posts, list_of(:forums_post) do
       resolve &ChinguCentral.Forums.PostResolver.all/2
     end
+    
+    field :forums_categories, list_of(:forums_category) do
+      resolve &ChinguCentral.Forums.CategoryResolver.all/2
+    end
+    
+    field :applications_cohorts, list_of(:applications_cohort) do
+      resolve &ChinguCentral.Applications.CohortResolver.all/2
+    end
   end
 end
