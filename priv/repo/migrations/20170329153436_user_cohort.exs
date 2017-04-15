fbfdfbff2ef983c161f3352c46cc899bf16c14f8
oxyrus <andresxz09@gmail.com>
2017-04-15 02:26:08 +0000

    Generate table to support many_to_many

--- priv/repo/migrations/20170329153436_user_cohort.exs
+++ priv/repo/migrations/20170329153436_user_cohort.exs
@@ -2,14 +2,14 @@ defmodule ChinguCentral.Repo.Migrations.UserCohort do
   use Ecto.Migration
 
   def change do
-    create table(:user_cohorts) do
-      add :user_id, references(:accounts_users, on_delete: :nothing), null: false
-      add :cohort_id, references(:applications_cohorts, on_delete: :nothing), null: false
+    create table(:applications_user_cohorts) do
+      add :accounts_users_id, references(:accounts_users, on_delete: :nothing), null: false
+      add :applications_cohorts_id, references(:applications_cohorts, on_delete: :nothing), null: false
 
       timestamps()
     end
-    create index(:user_cohorts, [:user_id])
-    create index(:user_cohorts, [:cohort_id])
-    create index(:user_cohorts, [:user_id, :cohort_id], unique: true)
+    create index(:applications_user_cohorts, [:accounts_users_id])
+    create index(:applications_user_cohorts, [:applications_cohorts_id])
+    create index(:applications_user_cohorts, [:accounts_users_id, :applications_cohorts_id], unique: true)
   end
 end
