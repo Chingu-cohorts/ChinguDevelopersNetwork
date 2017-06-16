21d3b1ea9cec5c94aa05d28b2eb64593e568847e
Oxyrus <im.oxyrus@gmail.com>
2017-06-16 15:39:33 -0500

    Write create user function
    There are tons of work to do, but at least it's something

--- controllers/users_controller.go
+++ controllers/users_controller.go
@@ -45,3 +45,59 @@ func ShowUser(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 
 	utils.JSONMessage(w, "User not found", http.StatusNotFound)
 }
+
+// CreateUser registers a new user in the database
+func CreateUser(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	db := utils.InitDB()
+	defer db.Close()
+
+	var user models.User
+
+	decoder := json.NewDecoder(r.Body)
+	err := decoder.Decode(&user)
+	if err != nil {
+		utils.JSONMessage(w, "Wrong body", http.StatusBadRequest)
+		return
+	}
+
+	if user.Username != "" && user.Email != "" {
+		// Used to check for an user with the same email
+		var existingUserWithEmail models.User
+		db.Where("email = ?", user.Email).First(&existingUserWithEmail)
+
+		// Used to check for an user with the same username
+		var existingUserWithUsername models.User
+		db.Where("username = ?", user.Username).First(&existingUserWithUsername)
+
+		// There's an user with the same email and username
+		if existingUserWithEmail.ID != 0 && existingUserWithUsername.ID != 0 {
+			utils.JSONMessage(w, "Username and email already taken", http.StatusOK)
+			return
+		}
+
+		// Email already registered
+		if existingUserWithEmail.ID != 0 {
+			utils.JSONMessage(w, "Email already registered", http.StatusOK)
+			return
+		}
+
+		// Username already registered
+		if existingUserWithUsername.ID != 0 {
+			utils.JSONMessage(w, "Username already registered", http.StatusOK)
+			return
+		}
+
+		// User was successfully created, format to JSON and response
+		db.Create(&user)
+
+		respBody, err := json.MarshalIndent(user, "", " ")
+		if err != nil {
+			log.Fatalf("Error formatting created user: %v", err)
+		}
+
+		utils.JSONResponse(w, respBody, http.StatusCreated)
+		return
+	}
+
+	utils.JSONMessage(w, "Email and Username are required", http.StatusBadRequest)
+}
