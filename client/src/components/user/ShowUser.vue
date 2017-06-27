47305c1b54ff6ac54bb11d9efdf20a862fb73290
Oxyrus <im.oxyrus@gmail.com>
2017-06-27 15:35:24 -0500

    Remove a bunch of mock data

--- client/src/components/user/ShowUser.vue
+++ client/src/components/user/ShowUser.vue
@@ -52,6 +52,7 @@
       <hr>
 
       <div class="columns feed">
+
         <div class="column is-one-quarter">
           <div class="content">
             <h2>About</h2>
@@ -63,7 +64,9 @@
             </a>
           </div>
         </div>
+
         <div class="column">
+
           <article class="media">
             <figure class="media-left">
               <p class="image is-64x64">
@@ -93,65 +96,9 @@
               </nav>
             </div>
           </article>
-          <article class="media">
-            <figure class="media-left">
-              <p class="image is-64x64">
-                <img src="http://lorempixel.com/128/128/">
-              </p>
-            </figure>
-            <div class="media-content">
-              <div class="content">
-                <p>
-                  <strong>John Smith</strong> <small>@johnsmith</small> <small>31m</small>
-                  <br>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ornare magna eros, eu pellentesque tortor vestibulum ut. Maecenas non massa sem. Etiam finibus odio quis feugiat facilisis.
-                </p>
-              </div>
-              <nav class="level is-mobile">
-                <div class="level-left">
-                  <a class="level-item">
-                    <span class="icon is-small"><i class="fa fa-reply"></i></span>
-                  </a>
-                  <a class="level-item">
-                    <span class="icon is-small"><i class="fa fa-retweet"></i></span>
-                  </a>
-                  <a class="level-item">
-                    <span class="icon is-small"><i class="fa fa-heart"></i></span>
-                  </a>
-                </div>
-              </nav>
-            </div>
-          </article>
-          <article class="media">
-            <figure class="media-left">
-              <p class="image is-64x64">
-                <img src="http://lorempixel.com/128/128/">
-              </p>
-            </figure>
-            <div class="media-content">
-              <div class="content">
-                <p>
-                  <strong>John Smith</strong> <small>@johnsmith</small> <small>31m</small>
-                  <br>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ornare magna eros, eu pellentesque tortor vestibulum ut. Maecenas non massa sem. Etiam finibus odio quis feugiat facilisis.
-                </p>
-              </div>
-              <nav class="level is-mobile">
-                <div class="level-left">
-                  <a class="level-item">
-                    <span class="icon is-small"><i class="fa fa-reply"></i></span>
-                  </a>
-                  <a class="level-item">
-                    <span class="icon is-small"><i class="fa fa-retweet"></i></span>
-                  </a>
-                  <a class="level-item">
-                    <span class="icon is-small"><i class="fa fa-heart"></i></span>
-                  </a>
-                </div>
-              </nav>
-            </div>
-          </article>
+
         </div>
+        
       </div>
     </div>
   </section>
@@ -180,6 +127,7 @@ export default {
       if (state.currentUser.projects) {
         return state.currentUser.projects
       }
+
       return '0'
     },
 
