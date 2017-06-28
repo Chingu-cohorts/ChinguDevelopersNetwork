81597e8b3c523bf3c1b90b0e97cc2a2f69728843
Oxyrus <im.oxyrus@gmail.com>
2017-06-28 13:07:23 -0500

    Fix json formatting

--- models/project.go
+++ models/project.go
@@ -8,7 +8,7 @@ type Project struct {
 	Name        string `gorm:"not_null" json:"name"`
 	Description string `gorm:"not_null" json:"description"`
 
-	Users []User `gorm:"many2many:user_projects"`
+	Users []User `gorm:"many2many:user_projects" json:"users,omitempty"`
 
 	CreatedAt time.Time  `json:"created_at"`
 	UpdatedAt time.Time  `json:"updated_at"`
