dc5500e3461e6503c7f521435dd515d2f724d95e
Oxyrus <im.oxyrus@gmail.com>
2017-07-06 21:39:43 -0500

    Start coding the forum components

---
+++ client/src/components/forum/IndividualPost.vue
@@ -0,0 +1,53 @@
+<template>
+<div class="box">
+  <div class="columns">
+    <article class="media">
+      <div class="column is-one-quarter-mobile is-2-desktop">
+        <div class="media-left">
+          <figure class="image is-64x64">
+            <img src="https://gravatar.com/avatar/3196ff4ca2acf9464de8a7c5b5e62957?s=512" alt="Image">
+          </figure>
+        </div>
+      </div>
+
+      <div class="column is-half-mobile is-8-desktop">
+        <div class="media-content">
+          <div class="content">
+            <p>
+              <a>The purpose of this project</a> <small>@oxyrus</small> <small>31m ago</small>
+              <br>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean efficitur sit amet massa fringilla egestas. Nullam condimentum luctus turpis.
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div class="column is one-quarter-mobile is-2-desktop">
+        <div class="media-right has-text-centered">
+          <h2>2K</h2>
+          <span>Comments</span>
+        </div>
+      </div>
+    </article>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'IndividualPost'
+}
+</script>
+
+<style scoped>
+img {
+  border-radius: 50%;
+}
+
+figure {
+  margin: 0 auto;
+}
+
+.media-right h2 {
+  font-weight: 700;
+}
+</style>
---
+++ client/src/components/forum/PostList.vue
@@ -0,0 +1,75 @@
+<template>
+<div>
+  <section class="hero is-medium is-dark is-bold">
+    <div class="hero-body">
+      <div class="container">
+        <h1 class="title">Forum</h1>
+        <h2 class="subtitle">Discuss ideas that matter</h2>
+      </div>
+    </div>
+  </section>
+  <div class="container">
+    <div class="columns forum-container">
+      <div class="column is-8">
+        <individual-post></individual-post>
+        <individual-post></individual-post>
+      </div>
+      <div class="column is-4">
+        <a class="box button is-primary is-outlined is-medium new-post">
+          <span class="icon is-small">
+            <i class="fa fa-check"></i>
+          </span>
+          <span>New Post</span>
+        </a>
+        <div class="box member-of-the-day">
+          <h2 class="has-text-centered is-primary">Member of the day</h2>
+          <img class="image is-128x128" src="https://gravatar.com/avatar/3196ff4ca2acf9464de8a7c5b5e62957?s=256">
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+import IndividualPost from './IndividualPost'
+
+export default {
+  name: 'PostList',
+
+  components: {
+    IndividualPost
+  },
+
+  computed: mapState([
+    'posts'
+  ])
+
+  /*
+  mounted () {
+    this.$store.dispatch('LOAD_POSTS_LIST')
+  }
+  */
+}
+</script>
+
+<style scoped>
+.forum-container {
+  margin-top: 2em;
+  margin-bottom: 2em;
+}
+
+.is-4 .box h2 {
+  font-weight: 700;
+}
+
+.member-of-the-day img {
+  border-radius: 50%;
+  margin: 0 auto;
+}
+
+.new-post {
+  display: flex;
+}
+</style>
