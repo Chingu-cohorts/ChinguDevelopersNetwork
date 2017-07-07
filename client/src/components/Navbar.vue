a2fda9fcb614e621c4d1d89f8873a8349876f29b
Oxyrus <im.oxyrus@gmail.com>
2017-07-06 21:39:08 -0500

    Update to the latest version of bulma

--- client/src/components/Navbar.vue
+++ client/src/components/Navbar.vue
@@ -1,72 +1,62 @@
 <template>
-  <nav class="nav has-shadow">
-    <div class="container">
-      <div class="nav-left">
-        <router-link
-          :to="{name: 'Hello'}"
-          class="nav-item">
-          <img src="https://i.imgur.com/CH034bi.png" alt="The Zoo">
-        </router-link>
-        <router-link
-          :to="{ name: 'CohortList'}"
-          exact-active-class="is-active"
-          class="nav-item is-tab is-hidden-mobile">
-          Cohorts
-        </router-link>
-        <router-link
-          :to="{ name: 'UserList'}"
-          exact-active-class="is-active"
-          class="nav-item is-tab is-hidden-mobile">
-          Users
-        </router-link>
-        <a class="nav-item is-tab is-hidden-mobile">Forums</a>
-        <a class="nav-item is-tab is-hidden-mobile">Projects</a>
-      </div>
-      <span class="nav-toggle" v-on:click="collapse">
-        <span></span>
-        <span></span>
-        <span></span>
-      </span>
-      <div class="nav-right nav-menu" v-bind:class="{'is-active': collapsed}">
-        <router-link
-          :to="{name: 'Hello'}"
-          class="nav-item is-tab is-hidden-tablet"
-          exact-active-class="is-active">
-          Home
-        </router-link>
-        <router-link
-          :to="{name: 'CohortList'}"
-          class="nav-item is-tab is-hidden-tablet"
-          exact-active-class="is-active">
-          Cohorts
-        </router-link>
-        <router-link
-          :to="{name: 'UserList'}"
-          class="nav-item is-tab is-hidden-tablet"
-          exact-active-class="is-active">
-          Users
-        </router-link>
-        <a class="nav-item is-tab is-hidden-tablet">Forums</a>
-        <a class="nav-item is-tab is-hidden-tablet">Projects</a>
+<nav class="navbar is-transparent">
+  <div class="navbar-brand">
+    <router-link
+      :to="{name: 'Hello'}"
+      class="navbar-item">
+      <img src="https://i.imgur.com/CH034bi.png" alt="The Zoo">
+    </router-link>
 
-        <template v-if="loggedUser.username">
-          <a class="nav-item is-tab" @click="reloadUser">{{ loggedUser.username }}</a>
-        </template>
-        <template v-else>
-          <router-link
-            :to="{name: 'SignIn'}"
-            class="nav-item is-tab">
-            Login
-          </router-link>
-          <router-link
-            :to="{name: 'SignUp'}"
-            class="nav-item is-tab">
-            Register
-          </router-link>
-        </template>
-      </div>
+    <a class="navbar-item is-hidden-desktop" href="https://github.com/Chingu-cohorts/ChinguCentral" target="_blank">
+      <span class="icon" style="color: #333;">
+        <i class="fa fa-github"></i>
+      </span>
+    </a>
+
+    <a class="navbar-item is-hidden-desktop" href="https://twitter.com/ChinguCollabs" target="_blank">
+      <span class="icon" style="color: #55acee;">
+        <i class="fa fa-twitter"></i>
+      </span>
+    </a>
+
+    <div class="navbar-burger burger" data-target="navMenu" @click="collapse">
+      <span></span>
+      <span></span>
+      <span></span>
     </div>
-  </nav>
+  </div>
+
+  <div id="navMenu" class="navbar-menu" v-bind:class="{'is-active': collapsed}">
+    <div class="navbar-start">
+      <router-link
+        :to="{ name: 'CohortList'}"
+        exact-active-class="is-active"
+        class="navbar-item">Cohorts</router-link>
+      <router-link
+        :to="{ name: 'UserList'}"
+        exact-active-class="is-active"
+        class="navbar-item">Users</router-link>
+      <router-link
+        :to="{ name: 'PostList'}"
+        exact-active-class="is-active"
+        class="navbar-item">Forum</router-link>
+    </div>
+
+    <div class="navbar-end">
+      <template v-if="loggedUser.username">
+        <a class="navbar-item" @click="reloadUser">{{ loggedUser.username }}</a>
+      </template>
+      <template v-else>
+        <router-link
+          :to="{name: 'SignIn'}"
+          class="navbar-item">Login</router-link>
+        <router-link
+          :to="{name: 'SignUp'}"
+          class="navbar-item">Register</router-link>
+      </template>
+    </div>
+  </div>
+</nav>
 </template>
 
 <script>
@@ -105,3 +95,9 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+.navbar {
+  border-bottom: 1px solid #ececec;
+}
+</style>
