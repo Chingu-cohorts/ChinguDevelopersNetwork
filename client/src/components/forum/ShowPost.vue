f15e7995e746a4b6a019b6c4295e29a7d80fbcfe
Oxyrus <im.oxyrus@gmail.com>
2017-07-09 10:59:18 -0500

    Just a barebones template

---
+++ client/src/components/forum/ShowPost.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1>{{ stuff() }}</h1>
+</template>
+
+<script>
+export default {
+  name: 'ShowPost',
+
+  methods: {
+    stuff () {
+      let { id, slug } = this.$route.params
+      return id + slug
+    }
+  }
+}
+</script>
