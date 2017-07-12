3df6dede270d5e7a29dd7349ad0007f5750b095a
Oxyrus <im.oxyrus@gmail.com>
2017-07-12 14:09:48 -0500

    Update deps

--- vendor/github.com/Chingu-cohorts/ChinguCentral/models/claim.go
+++
@@ -1,8 +0,0 @@
-package models
-
-import jwt "github.com/dgrijalva/jwt-go"
-
-type Claim struct {
-	User `json:"user"`
-	jwt.StandardClaims
-}
--- vendor/github.com/Chingu-cohorts/ChinguCentral/models/response_token.go
+++
@@ -1,6 +0,0 @@
-package models
-
-// ResponseToken represents a signed JWT token
-type ResponseToken struct {
-	Token string `json:"token"`
-}
