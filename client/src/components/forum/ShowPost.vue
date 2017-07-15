efc7dd981c03ae273d6d20ef1ce10b77e505135d
Oxyrus <im.oxyrus@gmail.com>
2017-07-15 12:39:29 -0500

    Use spinner component

--- client/src/components/forum/ShowPost.vue
+++ client/src/components/forum/ShowPost.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container">
     <template v-if="!currentPost.title">
-      <div class="loader">Loading...</div>
+      <spinner></spinner>
     </template>
     <template v-else>
       <div class="forum-post">
@@ -37,13 +37,15 @@
 <script>
 import MarkdownIt from 'markdown-it'
 import NewComment from '@/components/forum/NewComment'
+import Spinner from '@/components/Spinner'
 import { gravatar } from '@/components/utils'
 
 export default {
   name: 'ShowPost',
 
   components: {
-    NewComment
+    NewComment,
+    Spinner
   },
 
   methods: {
@@ -99,65 +101,5 @@ figure img {
 
 .username {
   font-weight: 700;
-}
-
-.loader,
-.loader:before,
-.loader:after {
-  background: #00d1b2;
-  -webkit-animation: load1 1s infinite ease-in-out;
-  animation: load1 1s infinite ease-in-out;
-  width: 1em;
-  height: 4em;
-}
-.loader {
-  color: #00d1b2;
-  text-indent: -9999em;
-  margin: 88px auto;
-  position: relative;
-  font-size: 11px;
-  -webkit-transform: translateZ(0);
-  -ms-transform: translateZ(0);
-  transform: translateZ(0);
-  -webkit-animation-delay: -0.16s;
-  animation-delay: -0.16s;
-}
-.loader:before,
-.loader:after {
-  position: absolute;
-  top: 0;
-  content: '';
-}
-.loader:before {
-  left: -1.5em;
-  -webkit-animation-delay: -0.32s;
-  animation-delay: -0.32s;
-}
-.loader:after {
-  left: 1.5em;
-}
-@-webkit-keyframes load1 {
-  0%,
-  80%,
-  100% {
-    box-shadow: 0 0;
-    height: 4em;
-  }
-  40% {
-    box-shadow: 0 -2em;
-    height: 5em;
-  }
-}
-@keyframes load1 {
-  0%,
-  80%,
-  100% {
-    box-shadow: 0 0;
-    height: 4em;
-  }
-  40% {
-    box-shadow: 0 -2em;
-    height: 5em;
-  }
 }
 </style>
