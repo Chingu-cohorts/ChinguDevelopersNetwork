3e3ed12710ff22d8cf51e078e091b12fc86204ff
Oxyrus <im.oxyrus@gmail.com>
2017-07-15 12:29:05 -0500

    Create basic new comment template

---
+++ client/src/components/forum/NewComment.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <template v-if="!loggedUser.id">
+      <em>You must login to be able to comment</em>
+    </template>
+    <template v-else>
+      <markdown-editor v-model="comment.content" ref="markdownEditor"></markdown-editor>
+    </template>
+  </div>
+</template>
+
+<script>
+import { markdownEditor } from 'vue-simplemde'
+
+export default {
+  name: 'new-comment',
+
+  components: {
+    markdownEditor
+  },
+
+  data () {
+    return {
+      comment: {
+        content: ''
+      }
+    }
+  },
+
+  computed: {
+    loggedUser () {
+      return this.$store.state.user.loggedUser
+    }
+  }
+}
+</script>
