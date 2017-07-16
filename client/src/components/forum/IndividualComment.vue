b2cd5e6a7d7ae602e6934de79023dd12a5a51d30
Oxyrus <im.oxyrus@gmail.com>
2017-07-16 12:55:56 -0500

    Start writing individual comment component

---
+++ client/src/components/forum/IndividualComment.vue
@@ -0,0 +1,37 @@
+<template>
+<article class="media">
+  <figure class="media-left">
+    <p class="image is-64x64">
+      <img src="http://bulma.io/images/placeholders/128x128.png">
+    </p>
+  </figure>
+  <div class="media-content">
+    <div class="content">
+      <p>
+        <strong>Barbara Middleton</strong>
+        <br>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis porta eros lacus, nec ultricies elit blandit non. Suspendisse pellentesque mauris sit amet dolor blandit rutrum. Nunc in tempus turpis.
+        <br>
+        <small><a>Like</a> · <a>Reply</a> · 3 hrs</small>
+      </p>
+    </div>
+  </div>
+</article>  
+</template>
+
+<script>
+import { gravatar } from '@/components/utils'
+
+export default {
+  name: 'individual-comment',
+
+  props: ['comment'],
+
+  computed: {
+    userGravatar (props) {
+      let { email } = props.comment.user
+      return gravatar(email)
+    }
+  }
+}
+</script>
