3cce977a7fb7c34cdf668ff8fe927cd95c73842a
Oxyrus <im.oxyrus@gmail.com>
2017-07-16 12:56:29 -0500

    Rewrite sign in component

--- client/src/components/user/SignIn.vue
+++ client/src/components/user/SignIn.vue
@@ -1,21 +1,14 @@
 <template>
-<div>
-  <section class="hero is-primary">
-    <div class="hero-body">
-      <div class="container">
-        <h1 class="title">Sign In</h1>
-        <h2 class="subtitle">Start interacting with other members</h2>
-      </div>
-    </div>
-  </section>
-
-  <section class="section">
-    <div class="columns">
-      <div class="container">
-        <div class="column is-half is-offset-one-quarter">
+<section class="section signin">
+  <div class="columns">
+    <div class="container">
+      <div class="column is-half is-offset-one-quarter signin-container">
+        <h1 class="title has-text-centered">Login</h1>
+        <div class="form-container">
+          <p class="has-text-centered description">Start interacting with the community.</p>
           <div class="field">
             <p class="control has-icons-left">
-              <input class="input" type="text" placeholder="Username" v-model="user.username">
+              <input class="input" type="text" placeholder="Username" v-model="user.username" required>
               <span class="icon is-small is-left">
                 <i class="fa fa-user"></i>
               </span>
@@ -23,7 +16,7 @@
           </div>
           <div class="field">
             <p class="control has-icons-left">
-              <input class="input" type="password" placeholder="Password" v-model="user.password">
+              <input class="input" type="password" placeholder="Password" v-model="user.password" required>
               <span class="icon is-small is-left">
                 <i class="fa fa-lock"></i>
               </span>
@@ -31,7 +24,7 @@
           </div>
           <div class="field">
             <p class="control">
-              <button class="button is-primary is-outlined" type="submit" v-on:click="loginUser">
+              <button class="button is-primary" type="submit" @click="loginUser">
                 Login
               </button>
             </p>
@@ -39,8 +32,8 @@
         </div>
       </div>
     </div>
-  </section>
-</div>
+  </div>
+</section>
 </template>
 
 <script>
@@ -88,3 +81,66 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+.signin {
+  background: url(/static/images/signin-bg.jpg) no-repeat center center fixed;
+  background-size: cover;
+  height: 100%;
+  min-height: 80vh;
+}
+
+.signin-container {
+  background-color: rgba(0, 0, 0, .3);
+  border-radius: 0.5em;
+  margin-top: 2em;
+  color: #f9f9f9;
+}
+
+.signin-container h1 {
+  font-weight: 700;
+  text-transform: uppercase; 
+  color: #f9f9f9;
+}
+
+.signin-container .fa {
+  color: #f9f9f9;
+}
+
+.signin-container input {
+  background-color: rgba(255, 255, 255, .15);
+  border: none;
+  box-shadow: none;
+  border-radius: 0;
+  color: #f9f9f9;
+  text-shadow: -2px 0px 1px #000;
+}
+
+.signin-container input:focus {
+  background-color: rgba(255, 255, 255, .3);
+  border: 1px solid #15df89;
+}
+
+.signin-container input::-webkit-input-placeholder {
+  color: #f9f9f9;
+}
+
+.signin-container .form-container {
+  width: 80%;
+  margin: 0 auto;
+}
+
+.signin-container .form-container .description {
+  margin-bottom: 1em;
+}
+
+.signin-container .form-container .button {
+  display: flex;
+  margin: 0 auto;
+}
+
+.control.has-icons-left .input,
+.control.has-icons-left .select select {
+  padding-left: 2.4em;
+}
+</style>
