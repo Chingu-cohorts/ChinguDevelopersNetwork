c846234e61d2aa981574078acef1bc635d3a7ac7
Oxyrus <im.oxyrus@gmail.com>
2017-07-20 15:43:24 -0500

    Autoprefix CSS

--- client/.postcssrc.js
+++ client/.postcssrc.js
@@ -3,6 +3,8 @@
 module.exports = {
   "plugins": {
     // to edit target browsers: use "browserlist" field in package.json
-    "autoprefixer": {}
+    "autoprefixer": {
+      browsers: ['last 2 versions']
+    }
   }
 }
