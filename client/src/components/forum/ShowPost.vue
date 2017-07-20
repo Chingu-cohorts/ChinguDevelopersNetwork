3ba0d07f304aef2a2ae0e0d707fd0d04c286cb81
Oxyrus <im.oxyrus@gmail.com>
2017-07-20 15:44:59 -0500

    Create a post container

--- client/src/components/forum/ShowPost.vue
+++ client/src/components/forum/ShowPost.vue
@@ -1,42 +1,50 @@
 <template>
   <div class="container">
-    <template v-if="!currentPost.title">
-      <spinner></spinner>
-    </template>
-    <template v-else>
-      <div class="forum-post">
-        <div class="columns">
-          <div class="column is-hidden-mobile is-2-desktop">
-            <div class="box has-text-centered user-box">
-              <figure class="image is-128x128">
-                <img :src="userGravatar">
-              </figure>
-              <div>
-                <a class="username">{{ currentPost.user.username }}</a>
+    <div class="post-container">
+      <template v-if="!currentPost.title">
+        <spinner></spinner>
+      </template>
+      <template v-else>
+        <div class="forum-post">
+          <div class="columns">
+            <div class="column is-hidden-mobile is-2-desktop">
+              <div class="box has-text-centered user-box">
+                <figure class="image is-128x128">
+                  <img :src="userGravatar">
+                </figure>
+                <div>
+                  <a class="username" v-bind:class="{ admin: currentPost.user.is_admin }">{{ currentPost.user.username }}</a>
+                </div>
               </div>
             </div>
-          </div>
 
-          <div class="column is-12-mobile is-10-desktop">
-            <div class="box">
-              <div class="content">
-                <h1 class="has-text-centered">{{ currentPost.title }}</h1>
-                <hr>
-                <div v-html="content">{{ currentPost.content }}</div>
+            <div class="column is-12-mobile is-10-desktop">
+              <div class="box">
+                <div class="content">
+                  <h1 class="has-text-centered">{{ currentPost.title }}</h1>
+                  <hr>
+                  <div v-html="content">{{ currentPost.content }}</div>
+                </div>
               </div>
             </div>
           </div>
         </div>
+      </template>
+
+      <individual-comment
+        v-for="comment in currentPost.comments"
+        :comment="comment"
+        :key="comment.id">
+      </individual-comment>
+
+      <div class="new-comment" v-if="!currentPost.isClosed">
+        <new-comment></new-comment>
       </div>
-    </template>
-
-    <individual-comment
-      v-for="comment in currentPost.comments"
-      :comment="comment"
-      :key="comment.id">
-    </individual-comment>
-
-    <new-comment></new-comment>
+      <div class="post-closed" v-else>
+        <p>This post is closed.</p>
+      </div>
+    
+    </div>  
   </div>
 </template>
 
@@ -89,7 +97,7 @@ export default {
     let { id } = this.$route.params
     this.$store.dispatch('LOAD_FORUM_POST', id)
 
-    this.interval = setInterval(() => this.loadPost(), 30000)
+    this.interval = setInterval(() => this.loadPost(), 15000)
   },
 
   beforeDestroy () {
@@ -99,6 +107,17 @@ export default {
 </script>
 
 <style scoped>
+.container {
+  margin-top: 2em;
+  margin-bottom: 2em;
+}
+
+.post-container {
+  background-color: #f9f9f9;
+  padding: 0 0.5em 1em;
+  border-radius: 0.2em;
+}
+
 hr {
   width: 40%;
   margin: 0 auto;
@@ -117,10 +136,15 @@ figure img {
 .user-box {
   border-radius: 0;
   box-shadow: none;
+  background-color: transparent;
 }
 
 .username {
   font-weight: 700;
+}
+
+.admin {
+  color: #28a0dc;
 }
 
 .content {
