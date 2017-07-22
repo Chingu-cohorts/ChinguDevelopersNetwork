5a23b6f2bfbe3bb6200fb1647f91d2e6d0e1af58
Oxyrus <im.oxyrus@gmail.com>
2017-07-22 17:30:10 -0500

    Write db settings for heroku

--- utils/db.go
+++ utils/db.go
@@ -2,6 +2,7 @@ package utils
 
 import (
 	"log"
+	"os"
 
 	"github.com/jinzhu/gorm"
 	// Required to use PostgreSQL
@@ -10,16 +11,21 @@ import (
 
 // InitDB returns a connection to the database
 func InitDB() *gorm.DB {
-	config, err := LoadSettings("config.json")
-	if err != nil {
-		log.Fatalf("Error loading config file for the database: %v", err)
-	}
+	// config, err := LoadSettings("config.json")
+	// if err != nil {
+	//	log.Fatalf("Error loading config file for the database: %v", err)
+	// }
 
-	var dbParameters = "host=" + config.Database.Host +
-		" user=" + config.Database.User +
-		" dbname=" + config.Database.Name +
+	host := os.Getenv("DB_HOST")
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	dbname := os.Getenv("DB_NAME")
+
+	var dbParameters = "host=" + host +
+		" user=" + user +
+		" dbname=" + password +
 		" sslmode=disable" +
-		" password=" + config.Database.Password
+		" password=" + dbname
 
 	db, err := gorm.Open("postgres", dbParameters)
 	if err != nil {
