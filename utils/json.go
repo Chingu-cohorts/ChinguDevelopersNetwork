a3cc173801c4bcd3a94e5b17138cf6250220b848
Oxyrus <im.oxyrus@gmail.com>
2017-07-24 12:04:16 -0500

    Attempt to fix middleware issues

--- utils/json.go
+++ utils/json.go
@@ -32,7 +32,7 @@ func JSONMessage(w http.ResponseWriter, message string, code int) {
 
 // JSONResponse simplifies returning JSON formatted structures
 func JSONResponse(w http.ResponseWriter, json []byte, code int) {
-	w.Header().Set("Vary", "Accept-Encoding")
+	w.Header().Set("Vary", "Origin; Accept-Encoding")
 	w.Header().Set("Content-Encoding", "gzip")
 	w.Header().Set("Content-Type", "application/json; charset=utf-8")
 	w.WriteHeader(code)
