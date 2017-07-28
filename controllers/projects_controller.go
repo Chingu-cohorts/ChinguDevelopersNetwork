2f014e99af6519863684cc6b317162c975898e99
Oxyrus <im.oxyrus@gmail.com>
2017-07-28 15:57:19 -0500

    Add create project function

--- controllers/projects_controller.go
+++ controllers/projects_controller.go
@@ -47,3 +47,64 @@ func ShowProject(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 
 	utils.JSONMessage(w, "Project not found", http.StatusNotFound)
 }
+
+// CreateProject saves a new project for a given user
+func CreateProject(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	db := utils.InitDB()
+	defer db.Close()
+
+	// We need to get the user id from the token
+	// then we can proceed to load the user variable
+	// with the data for the user from the database
+	// and then we can append the new project
+
+	var project models.Project
+
+	decoder := json.NewDecoder(r.Body)
+	err := decoder.Decode(&project)
+	if err != nil {
+		utils.JSONMessage(w, "Wrong project body", http.StatusBadRequest)
+		return
+	}
+
+	token := r.Header.Get("Authorization")
+	tokenData, err := utils.ReadJWT(token)
+	if err != nil {
+		utils.JSONMessage(w, "Error reading token while creating post", http.StatusInternalServerError)
+		return
+	}
+
+	var user models.User
+	// Find the user in the db
+	db.First(&user, tokenData.User.ID)
+
+	// If we found the user we can append the project
+	if user.ID != 0 {
+		db.Model(&user).Association("Projects").Append(&project)
+		utils.JSONMessage(w, "Project saved successfully", http.StatusCreated)
+		return
+	}
+
+	utils.JSONMessage(w, "User not found", http.StatusNotFound)
+}
+
+// AppendUser adds a new user to the project
+func AppendUser(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	db := utils.InitDB()
+	defer db.Close()
+
+	// ToDo: I have to create an struct which includes both
+	// the project id and the user id (or some sort of similar)
+	// unique identifiers
+
+	// We have to find the project in the database so we can append an user to it
+	var project models.Project
+	db.First(&project, ps.ByName("projectID"))
+
+	// If we found the project then we can proceed
+	if project.ID != 0 {
+		// Do stuff
+	}
+
+	utils.JSONMessage(w, "Could not append project to user", http.StatusInternalServerError)
+}
