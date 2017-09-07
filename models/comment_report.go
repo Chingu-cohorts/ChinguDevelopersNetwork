027271e243d3779579560334d2a0d0103a403f60
Oxyrus <im.oxyrus@gmail.com>
2017-09-07 16:17:20 -0500

    Add is closed field

--- models/comment_report.go
+++ models/comment_report.go
@@ -5,6 +5,7 @@ type CommentReport struct {
 	BaseModel
 
 	Description string `gorm:"not null" json:"description"`
+	IsClosed    bool   `gorm:"default:false" json:"is_closed"`
 
 	Comment   Comment `json:"comment,omitempty"`
 	CommentID uint    `gorm:"not null" json:"comment_id"`
--- models/post_report.go
+++ models/post_report.go
@@ -5,6 +5,7 @@ type PostReport struct {
 	BaseModel
 
 	Description string `gorm:"not null" json:"description"`
+	IsClosed    bool   `gorm:"default:false" json:"is_closed"`
 
 	Post   Post `json:"post,omitempty"`
 	PostID uint `gorm:"not null" json:"post_id"`
